0f2a382e53ae191d8db8a85143b8d243f28e5bd7
crandre <33586145+crandre@users.noreply.github.com>
2024-04-08 22:53:44 -0300

    Create equipaments.php

---
+++ equipaments.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+background-color: #ddd;
+font-family: Arial, sans-serif;
+}
+h1 {
+color: green;
+text-align: center;
+padding: 20px;
+}
+
+h2 {
+color: black;
+text-align: center;
+padding: 15px;
+}
+
+ul {
+list-style-type: none;
+padding: 0;
+}
+
+li {
+margin-bottom: 10px;
+}
+
+</style>
+
+</head>
+<body>
+<h1>Tamandufit</h1>
+
+<h2>Equipamentos para o treino de hoje:</h2>
+
+<ul>
+<?php
+if (isset($_POST['muscles'])) {
+$muscles = $_POST['muscles'];
+$equipments = array(
+'peito' => array('Bench press', 'Dumbbell fly'),
+'Costas' => array('Deadlift', 'Pull-ups'),
+'Ombro' => array('Military press', 'Lateral raises'),
+'biceps' => array('Barbell curls', 'Hammer curls'),
+'triceps' => array('Tricep pushdowns', 'Dips'),
+'abs' => array('Crunches', 'Planks'),
+'Pernas' => array('Squats', 'Leg press')
+);
+foreach ($muscles as $muscle) {
+if (array_key_exists($muscle, $equipments)) {
+foreach ($equipments[$muscle] as $equipment) {
+echo "<li>". $equipment. "</li>";
+}
+}
+}
+} else {
+echo "<li>Nenhum grupo muscular selecionado.</li>";
+}
+?>
+</ul>
+</body>
+</html>
